340aa9fffe566e6ce68c90a09787f55838226d29
Andre <and.rezanovich@gmail.com>
2020-03-19 09:46:47 +0200

    import cleanup

--- ng-tryout/src/app/pages/todos/todos.component.ts
+++ ng-tryout/src/app/pages/todos/todos.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {DataServiceService, Todo} from '../../services/data-service.service';
-import {map} from 'rxjs/operators';
 
 
 @Component({
